9e55d5fd85549cc494041d741fff34f518867e9f
Rafaela Ribeiro <rapha11br@gmail.com>
2021-03-12 14:41:38 -0300

    Update nodemailer.js

--- pacote-download/backend/config/nodemailer.js
+++ pacote-download/backend/config/nodemailer.js
@@ -2,14 +2,6 @@ const nodemailer = require("nodemailer");
 const path = require("path");
 const hbs = require("nodemailer-express-handlebars");
 
-// const transport = nodemailer.createTransport({
-//   host: "smtp.mailtrap.io",
-//   port: 2525,
-//   auth: {
-//     user: "50aee5934d70e4",
-//     pass: "2b802d72591b42",
-//   },
-// });
 
 const transport = nodemailer.createTransport({
   host: process.env.HOST,
